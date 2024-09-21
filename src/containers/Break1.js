fdb1403664440355c82bd55cdb81b8cebff35fd7
Max Greenwald <maxsupergreenwald@gmail.com>
2024-09-21 17:29:44 -0400

    break1 now says ONE more to go not three lol

--- src/containers/Break1.js
+++ src/containers/Break1.js
@@ -38,7 +38,7 @@ class Break1 extends Component {
             <div className="text-container">
               <p className="Break-text">
                Great!
-                <br /><br /> Take a quick break. Three more to go!
+                <br /><br /> Take a quick break. One more to go!
                 <br /><br /> Remember: Continue to look carefully and do the best you can, rating your certainty as before.
                 <br /><br /> Please keep your eyes focused on the <b>cross in the center</b> (don't look at the circle where the stripes may be)
                 <br /><br /> The task will continue to be difficult, but it is okay to guess and it is okay to be uncertain.
