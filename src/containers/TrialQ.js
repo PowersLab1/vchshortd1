d4c2d5ebc18c07fd056654d57b49e9f5f333c2d4
Max Greenwald <maxsupergreenwald@gmail.com>
2024-10-05 15:03:52 -0400

    now beta is at 2 (still easy) and tguess is 0.85

--- src/containers/TrialQ.js
+++ src/containers/TrialQ.js
@@ -22,13 +22,13 @@ class TrialQ extends Component {
 
     // Initializing QUEST
     // NOTE: Modify your quest parameters here!
-    let tGuess1 = Math.log10(0.23), // but then -0.85 and 1.08 made the lines disappear?
+    let tGuess1 = -0.85, // OG was too easy Math.log10(0.23) -- but then -0.85 and 1.08 made the lines disappear?
       // tGuess2 = -1.08, //Math.log10(0.23), changed for mturk to make harder
       // tGuessSd = 0.05,
-      tGuess2 = Math.log10(0.23), // but then -0.85 and 1.08 made the lines disappear?
-      tGuessSd = 0.1//0.05, //originally 0.1 -- change to 0.05 to make harder
+      tGuess2 = -0.85, // but then -0.85 and 1.08 made the lines disappear?
+      tGuessSd = 0.1, //0.05, //originally 0.1 -- change to 0.05 to make harder
       pThreshold = 0.75,
-      beta = 2,
+      beta = 2, //3 = OG, changed to 2 which made it quite hard
       delta = 0.01,
       gamma = 0.01,
       grain = 0.001,
